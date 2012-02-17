fc0eeaa6ec97730cd99d15182969c9a9ed529175
Viktor Petersson <vpetersson@wireload.net>
2012-02-17 21:06:42 +0100

    Fixed bug with ifconfig and bringing down the alias

--- bin/ucarp_down.sh
+++ bin/ucarp_down.sh
@@ -5,7 +5,7 @@
 load_rc_config ucarp
  
 /usr/local/etc/rc.d/pgpool stop
-/sbin/ifconfig $ucarp_if -alias $ucarp_addr/32
+/sbin/ifconfig $ucarp_if -alias $ucarp_addr
 echo "Refusing to do go back online to avoid a split-brain situation."
 echo ""
 echo "Manually run 'pg_ha.sh slave' or 'pg_ha.sh slave-init' after ensuring the master got the latest data."
